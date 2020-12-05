57e7e82df17cdc86469b14f0fd5ede994fce9eff
Akhil S Kumar <54428228+akhil-s-kumar@users.noreply.github.com>
2020-12-05 10:37:06 +0530

    Create main.dart

---
+++ Data-Types-1/main.dart
@@ -0,0 +1,17 @@
+void main(){
+  // Initializing a variable a as a integer
+  int a = 5;
+  print(a);
+  
+  // Initializing a variable c as a decimal
+  double c = 4.85;
+  print(c);
+  
+  // Initializing a variable b as a string 
+  String b = "Akhil";
+  print(b);
+  
+  // Initializing a variable d as true or false
+  bool d = true;
+  print(d);
+}
