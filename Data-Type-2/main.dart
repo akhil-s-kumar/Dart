9aadce72141d2dbee08cbef2151d07449acabb9a
Akhil S Kumar <54428228+akhil-s-kumar@users.noreply.github.com>
2020-12-05 10:42:49 +0530

    Create main.dart

---
+++ Data-Type-2/main.dart
@@ -0,0 +1,9 @@
+void main(){
+ // var can store any type of data type, it will automatically assign to it.
+ var a = true;
+ print(a);
+  
+ // dynamic can store any type of data type
+  dynamic b = "Akhil";
+  print(b);
+}
